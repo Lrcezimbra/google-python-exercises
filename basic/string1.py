03ea5c48f9b2490d0c5ade3af67796bcd47ba49e
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2016-08-24 18:09:13 -0300

    string1 both_ends done

--- basic/string1.py
+++ basic/string1.py
@@ -29,13 +29,16 @@ def donuts(count):
     else:
         return 'Number of donuts: many'
 
+
 # B. both_ends
 # Given a string s, return a string made of the first 2
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    return
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
 
 
 # C. fix_start
